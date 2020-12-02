ccca42bf990e678f98e67ade6a85ffc1798d6d1e
wei chen <weichen9083@gmail.com>
2020-12-02 18:08:14 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -140,15 +140,8 @@ end
  
 
 def winner
-  WIN_COMBINATIONS.detect do |win_combo|
-    if (@board[win_combo[0]]) == "X" && (@board[win_combo[1]]) == "X" && (@board[win_combo[2]]) == "X"
-      return "X"
-    elsif (@board[win_combo[0]]) == "O" && (@board[win_combo[1]]) == "O" && (@board[win_combo[2]]) == "O"
-      return "O"
-    else
-      nil
-    end
-  end
+  x = won? 
+  @board[won[0]]
 end
 
 
